65c0bdb096c8963bea67213a7a713382f6c0bc6f
hq <hq_cml@163.com>
2016-03-25 18:26:37 +0800

    class2

---
+++ vendor/vendor2/MyClass2.php
@@ -0,0 +1,15 @@
+<?php
+namespace vendor\vendor2;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: didi
+ * Date: 16/3/25
+ * Time: 下午6:25
+ */
+class MyClass2 {
+    public function __construct()
+    {
+        echo "[vendor/vendor2/MyClass2.php] is loaded\n";
+    }
+}
