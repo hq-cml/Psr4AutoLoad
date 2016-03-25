c3422857604abb517977b97d4e0c29eddc6c5449
hq <hq_cml@163.com>
2016-03-25 18:28:54 +0800

    class2

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: didi
+ * Date: 16/3/25
+ * Time: 下午6:26
+ */
+include_once("./auto/Psr4AutoLoader.php");
+
+class App{
+    protected static $apppath = null;
+    private static function init(){
+        self::$apppath = realpath(dirname("./"));
+        $base_dir = self::$apppath.DIRECTORY_SEPARATOR."vendor".DIRECTORY_SEPARATOR;
+
+        $Autoload = new auto\Psr4AutoLoader();
+        $Autoload->addNamespace("vendor", $base_dir);
+        $Autoload->register();
+    }
+
+    public static function run(){
+        self::init();
+        $c = new vendor\vendor1\MyClass1();
+        new vendor\vendor2\MyClass2();
+
+        $c->hello();
+    }
+}
+
+App::run();
